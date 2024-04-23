0ba2dd0b82e9641e769161b7d33a96cb4e39f6e9
patilvijay9400 <159513041+vijayDFlake@users.noreply.github.com>
2024-04-23 15:40:49 +0530

    backend domain changed

--- client/src/pages/widgets/MyPostWidget.jsx
+++ client/src/pages/widgets/MyPostWidget.jsx
@@ -46,7 +46,7 @@ import {
         formData.append("picturePath", image.name);
       }
   
-      const response = await fetch(`https://social-mern-api.vercel.app/posts`, {
+      const response = await fetch(`https://social-media-api-kappa.vercel.app/posts`, {
         method: "POST",
         headers: { Authorization: `Bearer ${token}` },
         body: formData,
